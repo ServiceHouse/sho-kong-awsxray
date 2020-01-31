c2d43fd0235c73d5fece76886df4e83607c242dc
Ethem Cem Ozkan <ethemcem.ozkan@servicehouse.nl>
2020-01-31 15:04:07 +0100

    OPS-2696

--- handler.lua
+++ handler.lua
@@ -50,17 +50,38 @@ function SHOXrayPlugin:access(config)
     if not isempty(traceIdHeader) then
         local method =  kong.request.get_method()
         local url = kong.request.get_scheme() .. "://" .. kong.request.get_host() .. kong.request.get_path()
-        traceId = traceIdHeader:gsub("Root=", "")
+
+        local traceId = traceIdHeader:match("Root=(.+)")
+        local parentSegmentId = nil
+        local parentSegment = nil
+        if traceId:find(";Parent=") then
+            parentSegmentId = traceId:match("Parent=(.+)")
+            if parentSegmentId:find(";") then
+                k, l = parentSegmentId:find(";")
+                parentSegmentId = parentSegmentId:sub(0,k-1)
+            end
+            i, j = traceId:find(";Parent=")
+            traceId = traceId:sub(0,i-1)
+        end
+
+        if not parentSegmentId then
+            parentSegmentId = ""
+            parentSegment = ""
+        else
+            parentSegment =  ", \"parent_id\": \"".. parentSegmentId .."\""
+        end
+
         local startTime = ngx.now()
         local segmentId = tostring(generateId())
-
-        kong.ctx.plugin.segmentDoc = "\"trace_id\":\"" .. traceId .. "\", \"id\": \"" .. segmentId .. "\", \"start_time\": ".. tostring(startTime) .. ", \"name\": \"Kong\""
+        local subSegmentId = tostring(generateId())
+        kong.ctx.plugin.subSegmentDoc = "\"id\": \"" .. subSegmentId .. "\", \"start_time\": ".. tostring(startTime) .. ", \"name\": \"".. url.."\", \"namespace\": \"remote\""
+        kong.ctx.plugin.segmentDoc = "\"trace_id\":\"" .. traceId .. "\", \"id\": \"" .. segmentId .. "\", \"start_time\": ".. tostring(startTime) .. ", \"name\": \"Kong\", \"origin\": \"AWS::ECS::Container\"" .. parentSegment
         kong.ctx.plugin.httpPart = ",\"http\": {\"request\" : { \"url\" : \"".. url .."\", \"method\" : \"".. method .."\"}"
         local inProgress = ",\"in_progress\": true"
         local header = "{\"format\": \"json\", \"version\": 1}"
-        local traceData = header .. "\n" .. "{".. kong.ctx.plugin.segmentDoc .. kong.ctx.plugin.httpPart .."}" .. inProgress .. " }"
+        local traceData = header .. "\n" .. "{".. kong.ctx.plugin.segmentDoc .. kong.ctx.plugin.httpPart .."}, \"subsegments\": [{".. kong.ctx.plugin.subSegmentDoc .. inProgress .."}]" .. inProgress .. " }"
 
-        ngx.req.set_header("X-Amzn-Trace-Id", traceIdHeader..";Parent="..segmentId..";Sampled=1")
+        ngx.req.set_header("X-Amzn-Trace-Id", "Root=" .. traceId..";Parent="..subSegmentId..";Sampled=1")
         sendUDP(false,traceData,config.xray_host,config.xray_port)
     end
 end
@@ -74,8 +95,9 @@ function SHOXrayPlugin:log(config)
 
         kong.ctx.plugin.httpPart = kong.ctx.plugin.httpPart .. ",\"response\" : { \"status\" : ".. response .." }}"
         kong.ctx.plugin.segmentDoc = kong.ctx.plugin.segmentDoc .. ",\"end_time\": " .. tostring(endTime) .. kong.ctx.plugin.httpPart
+        kong.ctx.plugin.subSegmentDoc = kong.ctx.plugin.subSegmentDoc .. ",\"end_time\": " .. tostring(endTime)
         local header = "{\"format\": \"json\", \"version\": 1}"
-        local traceData = header .. "\n" .. "{".. kong.ctx.plugin.segmentDoc .. " }"
+        local traceData = header .. "\n" .. "{".. kong.ctx.plugin.segmentDoc .. ", \"subsegments\": [{".. kong.ctx.plugin.subSegmentDoc .. "}] }"
 
         ngx.timer.at(0, sendUDP,traceData,config.xray_host,config.xray_port )
     end
